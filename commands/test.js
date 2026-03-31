5642fbed92e8b86fdcf878e27fa35541dd20b678
mokeprince3-creator <mokeprince3@gmail.com>
2026-03-31 18:15:39 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+import configmanager from "../utils/configmanager.js";
+
+const number = 243974491169
+configmanager.config.users[number] = {
+    sudoList: ['243974491169@s.whatsapp.net'],
+    tagAudioPath: "tag.mp3",
+    antilink: false,
+    response: true,
+    autoreact: false,
+    prefix: ".",
+    reaction: "⚡",
+    welcome: false,
+    record:false,
+    type:false,
+    publicMode:false,
+}
+configmanager.save()
+
+configmanager.premiums.premiumUser[`p`] = {
+    premium: number,
+} 
+configmanager.saveP()
